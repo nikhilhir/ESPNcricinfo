6133dea16461ac218dac5f2ce406bc2da135ccce
RAJENDRA YADAV <rjyadav0230@gmail.com>
2022-08-23 20:50:57 +0530

    Folder Structure

---
+++ src/Pages/MainRouter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const MainRouter = () => {
+  return (
+    <Routes>
+      <Route />
+    </Routes>
+  );
+};
+
+export default MainRouter;
